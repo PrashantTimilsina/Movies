7ec01590f9c46e3baa63f8853deebfa08be37a6f
PrashantTimilsina <157892231+PrashantTimilsina@users.noreply.github.com>
2024-08-19 19:51:42 +0545

    Update Movie.js

--- src/Movie.js
+++ src/Movie.js
@@ -14,7 +14,7 @@ export function Movie({
     function () {
       async function getMovieDetails() {
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         console.log(data);
